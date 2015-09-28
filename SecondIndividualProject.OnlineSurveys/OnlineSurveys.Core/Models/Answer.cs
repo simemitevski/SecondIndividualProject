ce54f51caac2f4c7ad48cfabb517bcf72f8b4727
sime <sime@email.com>
2015-09-28 22:24:02 +0200

    + tables for unswers

---
+++ SecondIndividualProject.OnlineSurveys/OnlineSurveys.Core/Models/Answer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineSurveys.Core.Models
+{
+    public class Answer
+    {
+        public Guid Id { get; set; }
+
+    }
+}
--- SecondIndividualProject.OnlineSurveys/OnlineSurveys.Core/Models/Question.cs
+++ SecondIndividualProject.OnlineSurveys/OnlineSurveys.Core/Models/Question.cs
@@ -6,11 +6,17 @@ using System.Threading.Tasks;
 
 namespace OnlineSurveys.Core.Models
 {
+    public enum TypeOfQuestion
+    {
+        Text, MultiChoice, OneChoice, DropDownOneSelect, StarRating, MatrihRating
+    }
     public class Question
     {
         public Guid Id { get; protected set; }
         public string TxtQuestion { get; set; }
-        public string TypeOfQuestion { get; set; }
+        public TypeOfQuestion TypeOfQuestion { get; set; }
         public virtual Survey Survey { get; set; }
+        public ICollection<TextAnswer> TxTextAnswers { get; set; }
+        public ICollection<Answer> Answers { get; set; } 
     }
 }
