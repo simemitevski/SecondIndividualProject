1bf1435d308c129976af14ce7b02d328a583f2a3
sime <sime@email.com>
2015-09-30 15:38:29 +0200

    Adding RoleRepository

---
+++ SecondIndividualProject.OnlineSurveys/OnlinceSurveys.Infrastructure/Repositories/RoleRepository.cs
@@ -0,0 +1,13 @@
+﻿using OnlineSurveys.Core.Interfaces;
+using OnlineSurveys.Core.Models;
+
+namespace OnlinceSurveys.Infrastructure.Repositories
+{
+    public class RoleRepository : BaseRepository<Role>, IRoleRepository
+    {
+        public RoleRepository(OnlineSurveysDbContext context) : base(context)
+        {
+            
+        }
+    }
+}
