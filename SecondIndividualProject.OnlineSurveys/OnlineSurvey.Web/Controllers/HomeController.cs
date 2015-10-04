71a9124bca15acc3849842f4f27d91ea577e64ce
sime <sime@email.com>
2015-10-04 14:06:42 +0200

    Resolving the service

--- SecondIndividualProject.OnlineSurveys/OnlineSurvey.Web/Controllers/HomeController.cs
+++ SecondIndividualProject.OnlineSurveys/OnlineSurvey.Web/Controllers/HomeController.cs
@@ -3,22 +3,23 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
 using System.Web;
+using System.Web.ApplicationServices;
 using System.Web.Mvc;
 using OnlinceSurveys.Infrastructure;
 using OnlinceSurveys.Infrastructure.Repositories;
-using OnlineSurvey.AppServices;
+using OnlineSurvey.ApplicationServices;
 
 namespace OnlineSurvey.Web.Controllers
 {
     public class HomeController : Controller
     {
-        protected RolesServices _roleService;
+        protected RoleServices _roleService;
         public HomeController()
         {
             string connString = ConfigurationManager.ConnectionStrings["OnlineSurveyDbConn"].ToString();
             var dbContext = new OnlineSurveyContext(connString);
             var roleRepo = new RoleRepository(dbContext);
-            _roleService = new RolesServices(roleRepo);
+            _roleService = new RoleServices(roleRepo);
 
         }
         public ActionResult Index()
